c20e2b592dd6e0ad2d2aa1ef03456b2e70f0ac1d
Hatim <hatimmurtuza10@gmail.com>
2020-09-10 23:42:57 +0300

    🚧 render posts

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,46 @@
 import { Navbar } from '../components/navbar';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../src/utils/createUrqlClient';
+import { usePostsQuery } from '../src/generated/graphql';
+import { Spin, List, Avatar } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 function Home() {
+	const [{ data }] = usePostsQuery();
+	const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 	return (
 		<div>
 			<Navbar />
+			{!data ? (
+				<Spin indicator={antIcon} />
+			) : (
+				<List
+					itemLayout="horizontal"
+					dataSource={data.posts}
+					renderItem={(post) => (
+						<List.Item>
+							<List.Item.Meta
+								title={<span style={{ marginTop: 20 }}>{post.title}</span>}
+								avatar={
+									<Avatar
+										style={{
+											backgroundColor: '#ed641a',
+											verticalAlign: 'middle',
+										}}
+										size="large"
+										gap={7}
+									>
+										{'U'}
+									</Avatar>
+								}
+							/>
+							{post.body}
+						</List.Item>
+					)}
+				/>
+			)}
 		</div>
 	);
 }
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Home);
+export default withUrqlClient(createUrqlClient)(Home);
